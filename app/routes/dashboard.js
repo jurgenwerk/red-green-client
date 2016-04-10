3903d3809521bb303edbf09fe0eaa188a50e50be
Matic Jurglic <matic@jurglic.si>
2016-04-10 13:30:52 +0200

    Transition to dashboard.overview

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -3,6 +3,15 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   session: Ember.inject.service(),
+  beforeModel(transition) {
+    // trigger AuthenticatedRouteMixin’s beforeModel
+    this._super(...arguments);
+
+    if (transition.targetName === "dashboard.index"){
+      transition.abort();
+      this.transitionTo('dashboard.overview');
+    }
+  },
   actions: {
     logout() {
       this.get('session').invalidate();
