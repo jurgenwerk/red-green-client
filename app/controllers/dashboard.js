630916d83f1e607136f52a9873cef7bfb1743cc2
Matic Jurglic <matic@jurglic.si>
2016-04-11 21:57:40 +0200

    Add previous/next actions for period

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,6 +1,19 @@
 import Ember from 'ember';
 import BalanceChangePropertiesMixin from 'red-green-client/mixins/balance-change-properties';
+import moment from 'moment';
 
 export default Ember.Controller.extend(BalanceChangePropertiesMixin, {
-  session: Ember.inject.service()
+  session: Ember.inject.service(),
+  queryParams: ['period'],
+  period: moment().format("YYYY-MM"), //default period
+  actions: {
+    goPeriodBack() {
+      const previousPeriod = moment(this.get('period')).subtract(1, "month").format("YYYY-MM");
+      this.set('period', previousPeriod);
+    },
+    goPeriodNext() {
+      const nextPeriod = moment(this.get('period')).add(1, "month").format("YYYY-MM");
+      this.set('period', nextPeriod);
+    }
+  }
 });
