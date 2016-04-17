6925157875588581528f44f31f8940ec88276480
Matic Jurglic <matic@jurglic.si>
2016-04-17 14:29:02 +0200

    Add liquid fire transition animation (index <-> edit/now)

---
+++ app/transitions.js
@@ -0,0 +1,26 @@
+export default function() {
+  this.transition(
+    this.fromRoute('dashboard.expenses.index'),
+    this.toRoute('dashboard.expenses.new'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+  this.transition(
+    this.fromRoute('dashboard.incomes.index'),
+    this.toRoute('dashboard.incomes.new'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+  this.transition(
+    this.fromRoute('dashboard.expenses.index'),
+    this.toRoute('dashboard.expenses.edit'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+  this.transition(
+    this.fromRoute('dashboard.incomes.index'),
+    this.toRoute('dashboard.incomes.edit'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+}
