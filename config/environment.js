ae4b5f4bda7540fdf93e1f7c93d4273af7d8bd3a
Matic Jurglic <matic@jurglic.si>
2016-04-17 19:23:49 +0200

    Enable development logging

--- config/environment.js
+++ config/environment.js
@@ -21,11 +21,11 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.LOG_RESOLVER = true;
+    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    ENV.APP.LOG_TRANSITIONS = true;
+    ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    ENV.APP.LOG_VIEW_LOOKUPS = true;
     ENV.serverURL = 'http://red-green-api.herokuapp.com';
   }
 
@@ -42,7 +42,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.serverURL = 'https://red-green-api.herokuapp.com/';
   }
 
   ENV.apiBaseURL = ENV.serverURL + '/' + ENV.apiNamespace;
