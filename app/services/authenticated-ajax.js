6932bda4498009203b65237c09227aed9c99a79c
Matic Jurglic <matic@jurglic.si>
2016-07-10 15:59:09 +0200

    newer version of ember-ajax demands declaring trustedHosts

--- app/services/authenticated-ajax.js
+++ app/services/authenticated-ajax.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 import AjaxService from 'ember-ajax/services/ajax';
+import ENV from "../config/environment";
 
 export default AjaxService.extend({
   session: Ember.inject.service(),
+  trustedHosts: [ENV.serverURL.split('//')[1]],
   headers: Ember.computed('session', function() {
     const headers = {};
     this.get('session').authorize('authorizer:application',
